3e44653ec3d8a04b5f16ec92ea8e70aa1495a838
Edrei Barajas <edrei.barajas14@gmail.com>
2025-03-27 08:21:05 -0600

    finishing home page

---
+++ scripts/matchPass.js
@@ -0,0 +1,29 @@
+const passwordInput = document.getElementById('password');
+const confirmationPasswordInput = document.getElementById('password-confirm');
+
+const submitButton = document.getElementById('submit');
+let formMessage = document.getElementById('form-message-pass');
+
+confirmationPasswordInput.addEventListener('input', () => {
+    const passwordValue = passwordInput.value;
+    const confirmationPasswordValue = confirmationPasswordInput.value;
+
+    if (passwordValue === confirmationPasswordValue) {
+        formMessage.textContent = "";
+    }
+    else {
+        formMessage.textContent = "*Passwords don't match*";
+    }
+});
+
+submitButton.addEventListener('click', () => {
+    const passwordValue = passwordInput.value;
+    const confirmationPasswordValue = confirmationPasswordInput.value;
+
+    if (passwordValue !== confirmationPasswordValue) {
+        passwordInput.value = "";
+        confirmationPasswordInput.value = "";
+        passwordInput.focus();
+        formMessage.textContent = "Please, try again. Passwords must match";
+    }
+});

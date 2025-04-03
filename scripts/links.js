3643111b26926f9e55abb06ea7271ee006de5b5b
Edrei Barajas <edrei.barajas14@gmail.com>
2025-04-03 09:05:53 -0600

    home page

---
+++ scripts/links.js
@@ -0,0 +1,32 @@
+const baseURL = "https://EdreiBarajas1407.github.io/wdd230/";
+const linksURL = "https://EdreiBarajas1407.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+
+    displayLinks(data.weeks);
+
+};
+
+function displayLinks(weeks) {
+    const listContainer = document.getElementById("list-container");
+
+    weeks.forEach(week => {
+        const li = document.createElement("li");
+        li.textContent = `${week.week}: `;
+
+        listContainer.appendChild(li);
+
+        week.links.forEach(link => {
+            const linkElement = document.createElement("a");
+            linkElement.setAttribute("href", link.url);
+            linkElement.setAttribute("target", "_blank");
+            linkElement.textContent = link.title;
+
+            li.appendChild(linkElement)
+
+        })
+    });
+
+}

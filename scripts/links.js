8beea43250cf5de7f751ba8aa17e7760b1639265
Edrei Barajas <edrei.barajas14@gmail.com>
2025-04-03 09:42:13 -0600

    data correction

--- scripts/links.js
+++ scripts/links.js
@@ -2,31 +2,39 @@ const baseURL = "https://EdreiBarajas1407.github.io/wdd230/";
 const linksURL = "https://EdreiBarajas1407.github.io/wdd230/data/links.json";
 
 async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
+    try {
+        const response = await fetch(linksURL);
+        if (!response.ok) throw new Error(`Error: ${response.status}`);
 
-    displayLinks(data.weeks);
-
-};
+        const data = await response.json();
+        displayLinks(data.weeks);
+    } catch (error) {
+        console.error("No access to the links:", error);
+    }
+}
 
 function displayLinks(weeks) {
-    const listContainer = document.getElementById("list-container");
+    const listContainer = document.getElementById('list-container');
 
     weeks.forEach(week => {
-        const li = document.createElement("li");
+        const li = document.createElement('li');
         li.textContent = `${week.week}: `;
 
-        listContainer.appendChild(li);
-
-        week.links.forEach(link => {
-            const linkElement = document.createElement("a");
-            linkElement.setAttribute("href", link.url);
-            linkElement.setAttribute("target", "_blank");
+        week.links.forEach((link, index) => {
+            const linkElement = document.createElement('a');
+            linkElement.setAttribute('href', link.url);
+            linkElement.setAttribute('target', '_blank');
             linkElement.textContent = link.title;
 
-            li.appendChild(linkElement)
+            li.appendChild(linkElement);
 
-        })
+            if (index < week.links.length - 1) {
+                li.appendChild(document.createTextNode(" | "));
+            }
+        });
+
+        listContainer.appendChild(li);
     });
-
 }
+
+getLinks();

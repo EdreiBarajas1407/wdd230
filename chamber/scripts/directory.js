9df076c6b3497e39a2440b2ca1e64994a82ef465
Edrei Barajas <edrei.barajas14@gmail.com>
2025-04-05 09:37:22 -0600

    save again

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,5 +1,17 @@
-const baseURL = "https://EdreiBarajas1407.github.io/wdd230/";
 const url = './data/members.json';
+
+async function generateDirectoryCards() {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error('Error al cargar el archivo JSON');
+
+        const data = await response.json();
+        console.log(data);
+        displayCards(data.companies);
+    } catch (error) {
+        console.error('Hubo un error:', error);
+    }
+}
 
 function displayCards(directoryMembers) {
     const cardsContainer = document.querySelector('.members-container');
@@ -10,12 +22,12 @@ function displayCards(directoryMembers) {
         <img src="${member.img}" alt="Image of ${member.name} business" loading="lazy"/>
         <h3>${member.name}</h3>
         <div class="directory-card-text">
-          <p>Address: ${member.address}</p>
-          <p>Phone: ${member.phoneNumber}</p>
-          <a href="${member.webUrl}" target="_blank">${member.webUrl}</a>
+            <p>Address: ${member.address}</p>
+            <p>Phone: ${member.phoneNumber}</p>
+            <a href="${member.webUrl}" target="_blank">${member.webUrl}</a>
         </div>
         `;
-
         cardsContainer.appendChild(li);
     });
 }
+generateDirectoryCards();

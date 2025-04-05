47706a5a2efb3cb805f2c0bf2807afac665a723c
Edrei Barajas <edrei.barajas14@gmail.com>
2025-04-05 09:18:51 -0600

    Changes

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,47 +1,31 @@
 const baseURL = "https://EdreiBarajas1407.github.io/wdd230/";
 const url = 'https://EdreiBarajas1407.github.io/wdd230/chamber/data/members.json';
-const cards = document.querySelector("#cards");
 
-async function getCompanyData() {
-    const response = await fetch(url);
+async function generateDirectoryCards() {
+    const response = await fetch(directoryURL);
     const data = await response.json();
 
-    displayCompanies(data.companies);
-}
+    console.log(data);
+    displayCards(data.members);
 
-const displayCompanies = (companies) => {
-    companies.forEach((company) => {
-        const card = document.createElement('section');
-        const nameElement = document.createElement('h2');
-        const addressElement = document.createElement('p');
-        const phoneElement = document.createElement('p');
-        const websiteElement = document.createElement('a');
-        const membershipElement = document.createElement('p');
-        const logo = document.createElement('img');
+};
 
-        nameElement.textContent = company.name;
-        addressElement.textContent = `Address: ${company.address}`;
-        phoneElement.textContent = `Phone: ${company.phoneNumber}`;
-        websiteElement.href = company.webUrl;
-        websiteElement.textContent = "Visit Website";
-        websiteElement.target = "_blank";
-        membershipElement.textContent = `Membership: ${company.membership}`;
+function displayCards(directoryMembers) {
+    const cardsContainer = document.querySelector('.members-container');
 
-        logo.setAttribute('src', company.img);
-        logo.setAttribute('alt', `Logo of ${company.name}`);
-        logo.setAttribute('loading', 'lazy');
-        logo.setAttribute('width', '340');
-        logo.setAttribute('height', '240');
+    directoryMembers.forEach(member => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <img src="${member.img}" alt="Image of ${member.name} business" loading="lazy"/>
+        <h3>${member.name}</h3>
+        <div class="directory-card-text">
+          <p>Address: ${member.address}</p>
+          <p>Phone: ${member.phone}</p>
+          <a href="${member.website}">${member.website}</a>
+        </div>
+  
+      `;
 
-        card.appendChild(nameElement);
-        card.appendChild(addressElement);
-        card.appendChild(phoneElement);
-        card.appendChild(websiteElement);
-        card.appendChild(membershipElement);
-        card.appendChild(logo);
-
-        cards.appendChild(card);
+        cardsContainer.appendChild(li);
     });
 }
-
-getCompanyData();
